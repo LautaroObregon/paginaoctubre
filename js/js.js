12a7aae92873e5cab89bed495d58bb2423725452
LautaroObregon <143416157+LautaroObregon@users.noreply.github.com>
2023-10-30 11:04:02 -0300

    Delete js directory

--- js/js.js
+++
@@ -1,9 +0,0 @@
-function alternarTexto(id) {
-    var elemento = document.getElementById(id);
-    if (elemento.style.display === "block" || elemento.style.display === "") {
-      elemento.style.display = "none"; 
-     } else {
-      elemento.style.display = "block";}
-  }
-  
-  
